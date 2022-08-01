5c067d2c6555cf7933f4d02df44ede12046f2c03
Alejandro Castro <acastro.ma1@gmail.com>
2022-08-01 18:22:54 -0400

    Agregar archivos de proyecto.

---
+++ ApiOpenBootcamp/Data/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ApiOpenBootcamp.Models.DataModels;
+
+namespace ApiOpenBootcamp.Data
+{
+    public class DataContext: DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<Users>? Users { get; set; }
+    }
+}
---
+++ ApiOpenBootcamp/Models/DataModels/Curso.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApiOpenBootcamp.Models.DataModels
+{
+    public class Curso
+    {
+        public string n { get; set; }
+    }
+}
